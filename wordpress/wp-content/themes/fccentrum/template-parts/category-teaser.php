a9b9dd013dfa6d3c449298bf310164ea8f8a0de1
CecileEnzler <71143085+CecileEnzler@users.noreply.github.com>
2022-03-07 10:38:40 +0100

    styles commit

--- wordpress/wp-content/themes/fccentrum/template-parts/category-teaser.php
+++ wordpress/wp-content/themes/fccentrum/template-parts/category-teaser.php
@@ -4,13 +4,12 @@ $category = $args['category'];
 $image = get_field('headerafbeelding', $category);
 ?>
 
-<ul class="categories-block">
-	<li class="cat">
-		<div class="category-pic">
-			<a href="<?php echo get_category_link( $category->term_id ); ?>" title="<?php echo $category->name; ?>">
-				<?php echo wp_get_attachment_image( $image, 'medium' ); ?>
-			</a>
-		</div>
-		<h2><a href="<?php echo get_category_link( $category->term_id ); ?>" title="<?php echo $category->name; ?>"><?php echo $category->name; ?></a></h2>
-	</li>
-</ul>
+
+<li class="cat">
+	<div class="category-pic">
+		<a href="<?php echo get_category_link( $category->term_id ); ?>" title="<?php echo $category->name; ?>">
+			<?php echo wp_get_attachment_image( $image, 'medium' ); ?>
+		</a>
+	</div>
+	<h2><a href="<?php echo get_category_link( $category->term_id ); ?>" title="<?php echo $category->name; ?>"><?php echo $category->name; ?></a></h2>
+</li>
