e873d11b10c1d6752286c99c551a6bc420a61ee9
bram de leeuw <bramdeleeuw@gmail.com>
2022-03-15 12:15:07 +0100

    carousel

--- wordpress/wp-content/themes/fccentrum/index.php
+++ wordpress/wp-content/themes/fccentrum/index.php
@@ -1,6 +1,9 @@
 <?php
 get_header();
 ?>
+
+
+
 <main id="site-content">
 	<div class="stories-container">
 		<?php
---
+++ wordpress/wp-content/themes/fccentrum/js/carousel.js
@@ -0,0 +1,6 @@
+(function( $ ) {
+
+	$(document).ready(function(){
+		$('#carousel').slick();
+	});
+})(jQuery);
