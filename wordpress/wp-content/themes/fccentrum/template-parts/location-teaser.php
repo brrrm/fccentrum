b4156ba21cea7bd0b03856ef423e260a38a7afbc
bram de leeuw <bramdeleeuw@gmail.com>
2022-03-16 10:10:30 +0100

    spots singular

--- wordpress/wp-content/themes/fccentrum/template-parts/location-teaser.php
+++ wordpress/wp-content/themes/fccentrum/template-parts/location-teaser.php
@@ -5,7 +5,7 @@ $image = get_field('headerafbeelding', $spot);
 $location = get_field('locatie', $spot);
 ?>
 
-<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>">
+<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>" data-singular="<?php echo (isset($args['singular']) && $args['singular'] === true)? 1 : 0; ?>" >
 	<div class="marker-inner">
 		<div class="spot-pic">
 			<a href="<?php echo get_category_link( $spot->term_id ); ?>" title="<?php echo $spot->name; ?>">
