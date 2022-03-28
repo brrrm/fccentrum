e0d944c15b6faf0591e46acfb8d991865a1f2eed
bram de leeuw <bramdeleeuw@gmail.com>
2022-03-28 14:44:55 +0200

    poster image voor video in slideshow

--- wordpress/wp-content/themes/fccentrum/js/carousel.js
+++ wordpress/wp-content/themes/fccentrum/js/carousel.js
@@ -1,6 +1,21 @@
 (function( $ ) {
 
 	$(document).ready(function(){
+		let videos  = $(".slide-video");
+		videos.on("click", function(){
+            var elm = $(this),
+                conts   = elm.contents(),
+                le      = conts.length,
+                ifr     = null;
+
+            for(var i = 0; i<le; i++){
+              if(conts[i].nodeType == 8) ifr = conts[i].textContent;
+            }
+
+            elm.addClass("playing").html(ifr);
+            elm.off("click");
+        });
+
 		$('#carousel').slick({
 			'prevArrow': $('#prevslide'),
 			'nextArrow': $('#nextslide')
--- wordpress/wp-content/themes/fccentrum/template-parts/carousel.php
+++ wordpress/wp-content/themes/fccentrum/template-parts/carousel.php
@@ -3,12 +3,28 @@
 $link = get_field('slide_link');
 $image = get_field('slide_afbeelding');
 $video = get_field('slide_video');
+if($video && $image){
+	preg_match('/src="(.+?)"/', $video, $matches);
+	$src = $matches[1];
+	// Add extra parameters to src and replace HTML.
+	$params = array(
+	    'autoplay'  => 1,
+	    'hd'        => 1,
+	);
+	$new_src = add_query_arg($params, $src);
+	$video = str_replace($src, $new_src, $video);
+}
 ?>
 
 <div class="carousel-slide">
 	<?php if($video){ ?>
-		<div class="slide-video">
-			<?php echo $video; ?>
+		<div class="slide-video <?php if($image){ echo 'with-preview'; } ?>">
+			<?php echo wp_get_attachment_image( $image, [2000, 1500], false, ['class' => 'video-poster'] ); ?>
+			<?php if($image){ ?>
+			<!-- <?php echo $video; ?> -->
+			<?php }else{ ?>
+				<?php echo $video; ?>
+			<?php } ?>
 		</div>
 	<?php }else{ ?>
 		<div class="slide-image">
