8579feccdff6119e5d9e3fc7bfe1415bb74d5f62
bram de leeuw <bramdeleeuw@gmail.com>
2022-03-29 11:50:33 +0200

    logo header bug in Safari opgelost

--- wordpress/wp-content/themes/fccentrum/js/scroll.js
+++ wordpress/wp-content/themes/fccentrum/js/scroll.js
@@ -2,42 +2,39 @@
 	let lastKnownScrollPosition = 0;
 	let ticking = false;
 	let htmlTopMargin = parseInt( $('html').css('marginTop'));
+	var headerPos;
 	
 	$(document).ready(function(){
 		htmlTopMargin = parseInt($('html').css('marginTop'));
-
+		headerPos = $('#site-header').offset();
 		$('#hamburger').click(function(e){
 			e.preventDefault();
 			console.log('click');
 			$('nav.primary-menu-wrapper').toggleClass('show');
 		});
-
-		
-	});
-	document.addEventListener('scroll', function(e) {
-	  if (!ticking) {
-	    window.requestAnimationFrame(function() {
-	      headerScrollEffect(window.scrollY);
-	      ticking = false;
-	    });
-
-	    ticking = true;
-	  }
+		document.addEventListener('scroll', function(e) {
+			if (!ticking) {
+				window.requestAnimationFrame(function() {
+					headerScrollEffect(window.scrollY);
+					ticking = false;
+				});
+				ticking = true;
+			}
+		});
 	});
 
 	function headerScrollEffect(scrollPos) {
-	  let headerPos = $('#site-header').offset();
-	  console.log(scrollPos);
-	  if(scrollPos < 5){
-	  	$('body').addClass('showHeader');
-	  	$('#site-header').css('top', htmlTopMargin);
-	  }else if(scrollPos < lastKnownScrollPosition){
-	  	$('body').addClass('showHeader');
-	  	$('#site-header').css('top', htmlTopMargin);
-	  }else{	  	
-	  	$('body').removeClass('showHeader');
-	  	$('#site-header').css('top', headerPos.top);
-	  }
-	  lastKnownScrollPosition = scrollPos;
+		//console.log(scrollPos);
+		if(scrollPos < 40){
+			$('body').addClass('showHeader');
+			$('#site-header').css('top', htmlTopMargin);
+		}else if(scrollPos < lastKnownScrollPosition){
+			$('body').addClass('showHeader');
+			$('#site-header').css('top', htmlTopMargin);
+		}else{	  	
+			$('body').removeClass('showHeader');
+			$('#site-header').css('top', headerPos.top);
+		}
+	  	lastKnownScrollPosition = scrollPos;
 	}
 })(jQuery);
